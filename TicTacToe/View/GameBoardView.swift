861c8213879a1a6fb7682a218fff2df1c180050a
Chaniel Yu <chaniel.yu@gmail.com>
2019-05-07 23:04:10 +1000

    draw basic chess item

--- TicTacToe/View/GameBoardView.swift
+++ TicTacToe/View/GameBoardView.swift
@@ -25,13 +25,40 @@ class GameBoardView: UIView {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
         let context = UIGraphicsGetCurrentContext()
+        context?.saveGState()
         context?.setFillColor(backGroudColor.cgColor)
         context?.fill(rect)
         context?.setStrokeColor(foreGroudColor.cgColor)
-        context?.setLineWidth(20)
-        context?.stroke(bounds)
-        context?.move(to: CGPoint(x: rect.minX,y: rect.minY))
-        context?.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        context?.setLineWidth(10)
+        context?.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        context?.addLine(to: CGPoint(x: rect.maxX,y: rect.maxY))
         context?.strokePath()
+        drawCircle(context: context, rect: rect)
+        drawCross(context: context, rect: rect)
+        context?.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        context?.addLine(to: CGPoint(x: rect.maxX,y: rect.minY))
+        context?.strokePath()
+        context?.restoreGState()
+    }
+
+    private func drawCircle(context: CGContext?, rect: CGRect) {
+        let bound = rect.insetBy(dx: rect.midX/5, dy: rect.midY/5)
+        context?.saveGState()
+        context?.setLineWidth(min(bound.width, bound.height)/10)
+        context?.addEllipse(in: bound)
+        context?.strokePath()
+        context?.restoreGState()
+    }
+
+    private func drawCross(context: CGContext?, rect: CGRect) {
+        let bound = rect.insetBy(dx: rect.midX/5, dy: rect.midY/5)
+        context?.saveGState()
+        context?.setLineWidth(min(bound.width, bound.height)/10)
+        context?.move(to: CGPoint(x: bound.minX, y: bound.minY))
+        context?.addLine(to: CGPoint(x: bound.maxX,y: bound.maxY))
+        context?.move(to: CGPoint(x: bound.minX, y: bound.maxY))
+        context?.addLine(to: CGPoint(x: bound.maxX,y: bound.minY))
+        context?.strokePath()
+        context?.restoreGState()
     }
 }
